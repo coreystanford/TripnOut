d019209a65f6f37fff22ce1da9fbb9a7557fbeb5
MDykeman <matthewdykeman1@gmail.com>
2015-06-09 10:07:43 -0400

    Removed info in authService.js file

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -1,8 +1,1 @@
-//get the logged in user
-authFactory.getUSer = function()  {
-  if (AuthToken.getToken())
-    return $http.get('/api/me', { cache: true });
-    else
-        return $q.reject({  message: 'User has no token.'});
 
-  };
