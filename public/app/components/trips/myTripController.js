22055f4189ebb6227a5897170ce990874348ee47
Corey Stanford <corey.michael.stanford@gmail.com>
2015-06-17 22:37:50 -0400

    Trip ui-sref state params // Trip directive

--- public/app/components/trips/myTripController.js
+++ public/app/components/trips/myTripController.js
@@ -1,20 +1,14 @@
 tripnoutApp.controller('myTripController', function($rootScope, $scope, $location, $stateParams, Auth, User) {
 
 	  //get info if a person is logged in
-    // !!!! is this set in rootScope??
   	$scope.loggedIn = Auth.isLoggedIn();
 
   	User.get($stateParams.user_id)
     .success(function(data) {
-      $scope.processing = false;
 
-      console.log(data);
+      console.log(data.trips);
+      $scope.trips = data.trips;
 
-      //clear the form
-      $scope.userData = {};
-
-      //bind the message from our API to $scope.message
-      $scope.message = data.message;
     });
 
 });
